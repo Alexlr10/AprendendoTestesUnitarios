b3e78a5b8dc86310e00d843396c86d4f48418332
Alex Lopes <alexlr10@hotmail.com>
2020-08-03 13:12:32 -0300

    teste a partir do arquivo

--- MyClassesTest/FileProcessTest.cs
+++ MyClassesTest/FileProcessTest.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyClasses;
 
 namespace MyClassesTest {
     [TestClass]
     public class FileProcessTest {
+        private const string BAD_FILE_NAME = @"C:\BadFileName.bat";
+        private string _GoodFileName;
         [TestMethod]
         public void FileNameDoesExists() { //Nome do meu arquivo existe
 
             //Instancia um objeto fp de FileProcess
             FileProcess fp = new FileProcess();
             bool fromCall;
-            //Verifica se existe ou nao o arquivo Regedit no diretorio passado
-            fromCall = fp.FileExists(@"C:\Windows\Regedit.exe");
+            SetGoodFileName();
+            File.AppendAllText(_GoodFileName, "Some text");
+
+            //Verifica se existe ou nao o arquivo no diretorio passado
+            fromCall = fp.FileExists(_GoodFileName);
+            File.Delete(_GoodFileName);
+
            //Verifica se o arquivo e True ou False
             Assert.IsTrue(fromCall);
+        }
+
+        public void SetGoodFileName() {
+            //Pega o conteudo do arquivo de configuraçao por meio da chave
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            //verifica se ele contem um appPath
+            if (_GoodFileName.Contains("[appPath]")) {
+                //executa um replace pegando o local da aplicaçao
+                _GoodFileName = _GoodFileName.Replace("[appPath]",
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
         }
 
         [TestMethod]
@@ -24,7 +44,7 @@ namespace MyClassesTest {
             FileProcess fp = new FileProcess();
             bool fromCall;
             //Verifica se existe ou nao o arquivo Regedit no diretorio passado
-            fromCall = fp.FileExists(@"C:\Regedit.exe");
+            fromCall = fp.FileExists(BAD_FILE_NAME);
             //Verifica se o arquivo e True ou False
             Assert.IsFalse(fromCall);
         }
